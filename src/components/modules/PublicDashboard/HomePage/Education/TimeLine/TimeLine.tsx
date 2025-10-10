9f1a63fad97cbaa734af3555f2e1fad756f4400d
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-10-10 06:00:39 +0600

    upadte education color

--- src/components/modules/PublicDashboard/HomePage/Education/TimeLine/TimeLine.tsx
+++ src/components/modules/PublicDashboard/HomePage/Education/TimeLine/TimeLine.tsx
@@ -9,13 +9,17 @@ const TimeLine = ({ education }: IProps) => {
   return (
     <div className="flex items-start md:items-center gap-4 ">
       <div className="flex items-center gap-4 w-auto md:w-[35%]  ">
-        <p className="w-[20px] h-[20px] rounded-full bg-green-400 relative right-[10px] top-3 md:top-0"></p>
-        <p className="bg-[#292929] w-[140px]   text-white py-2 px-4 rounded-2xl hidden md:flex justify-center">
+        <p className="w-[20px] h-[20px] rounded-full bg-gradient-to-tr from-[#4A6FA5] via-[#6A8FBF] to-[#9BB8E0] relative right-[10px] top-3 md:top-0"></p>
+        <p
+          className={`
+       bg-gradient-to-tr from-[#3657A6] via-[#4F77BE] to-[#7CA9E8]
+          w-[140px]   text-white py-2 px-4 rounded-2xl hidden md:flex justify-center`}
+        >
           {education?.year}
         </p>
       </div>
       <div className="flex flex-col gap-5 md:gap-0 w-auto md:w-[65%]">
-        <p className="bg-[#333333] text-white py-2 px-4 rounded-2xl  md:hidden">
+        <p className="bg-gradient-to-tr from-[#4A6FA5] via-[#6A8FBF] to-[#9BB8E0] text-white py-2 px-4 rounded-2xl  md:hidden">
           {education?.year}
         </p>
         <p className="text-xl font-bold pText">{education?.institute}</p>
